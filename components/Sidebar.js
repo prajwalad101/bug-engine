29e7e7fc887b18fce0bbb1179267ffb065a99240
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-19 14:27:46 +0545

    add basic styling to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,10 +8,10 @@ import { SidebarData } from "./SidebarData";
 function Sidebar() {
   return (
     <>
-      <div className="mt-3 ml-3">
-        <MdIcons.MdMenu size={35} />
+      <div className="flex justify-start items-center h-10">
+        <MdIcons.MdMenu size={35} className="ml-3" />
       </div>
-      <nav>
+      <nav className="bg-slate-100 w-56 h-[100vh] flex justify-center fixed top-0 -left-full transition-all">
         <ul>
           <li>
             <AiIcons.AiOutlineClose />
