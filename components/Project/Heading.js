62578e7cc40dfd4a75d78ac6973d0de43da81000
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-22 20:04:30 +0545

    changed styling of issue and heading component

--- components/Project/Heading.js
+++ components/Project/Heading.js
@@ -15,7 +15,7 @@ function Heading() {
   return (
     <div>
       <div className="flex items-end gap-4">
-        <h1 className="font-bold text-2xl">{project.name}</h1>
+        <h1 className="font-bold text-3xl">{project.name}</h1>
         <p className="text-gray-500 ">21 January, 2021</p>
       </div>
       <div className="flex items-center gap-3 mt-2 text-gray-600 hover:cursor-pointer">
--- components/Project/Issue.js
+++ components/Project/Issue.js
@@ -1,10 +1,10 @@
 function Issue({ issue }) {
   return (
-    <div className="border-2 rounded-md p-2 mb-3 shadow-md">
-      <p>{issue.name}</p>
+    <div className="border-2 rounded-md py-2 px-4 mb-3 shadow-md">
+      <p className="text-lg pb-[1px]">{issue.name}</p>
       <div className="text-gray-600">
-        <span className="text-sm ">{issue.submitter}, </span>
-        <span className="text-sm ">2 days ago</span>
+        <span className="text-sm">{issue.submitter}, </span>
+        <span className="text-sm">2 days ago</span>
         <p className="text-sm py-2 text-blue-300"># {issue.type}</p>
       </div>
     </div>
