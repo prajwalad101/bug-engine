4ab745da13c01464190704ecd26630e4e164b622
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-23 21:35:34 +0545

    change projectSchema by adding an issue sub schema

--- models/Project.js
+++ models/Project.js
@@ -1,5 +1,33 @@
 const mongoose = require("mongoose");
 
+const issueSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "An issue must have a name"],
+      unique: true,
+      trim: true,
+    },
+    submitter: {
+      type: String,
+      required: [true, "An issue must have a submitter"],
+    },
+    type: {
+      type: String,
+      required: [true, "An issue must have a type"],
+    },
+    status: {
+      type: String,
+      required: [true, "An issue must have a status"],
+      enum: {
+        values: ["no status", "in progress", "completed"],
+        message: "Status should be either no status, in progress or completed",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
 const projectSchema = new mongoose.Schema(
   {
     name: {
@@ -8,39 +36,7 @@ const projectSchema = new mongoose.Schema(
       required: [true, "A project must have a name"],
       trim: true,
     },
-    createdOn: {
-      type: Date,
-      required: [true, "A project must have a date"],
-    },
-    issues: {
-      name: {
-        type: String,
-        required: [true, "An issue must have a name"],
-        unique: true,
-        trim: true,
-      },
-      submitter: {
-        type: String,
-        required: [true, "An issue must have a submitter"],
-      },
-      submittedOn: {
-        type: Date,
-        required: [true, "An issue must have a submitted date"],
-      },
-      type: {
-        type: String,
-        required: [true, "An issue must have a type"],
-      },
-      status: {
-        type: String,
-        required: [true, "An issue must have a status"],
-        enum: {
-          values: ["no status", "in progress", "completed"],
-          message:
-            "Status should be either no status, in progress or completed",
-        },
-      },
-    },
+    issues: [issueSchema],
   },
   { timestamps: true }
 );
