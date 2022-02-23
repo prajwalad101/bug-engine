aff88dfb7cc434a9383135c9f450c8c8d3b12cd6
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-23 11:11:03 +0545

    change the modal to add an issue

--- components/Modal/IssueModal.js
+++ components/Modal/IssueModal.js
@@ -1,8 +1,27 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import { useRouter } from "next/router";
+
+import { projects } from "../../dev-data/projects";
+import { getProjectById } from "../../utils/projectFunc";
 
 export default function IssueModal() {
-  let [isOpen, setIsOpen] = useState(true);
+  const router = useRouter();
+
+  const id = router.query.id;
+  const project = getProjectById(id);
+
+  const [isOpen, setIsOpen] = useState(true);
+  const [issue, setIssue] = useState("");
+
+  const handleChange = (event) => {
+    setIssue(event.target.value);
+  };
+
+  const addIssue = () => {
+    project.issues.push({ name: issue, submitter: "Michael Scott" });
+    closeModal();
+  };
 
   function closeModal() {
     setIsOpen(false);
@@ -14,16 +33,6 @@ export default function IssueModal() {
 
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <button
-          type="button"
-          onClick={openModal}
-          className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-          Open dialog
-        </button>
-      </div>
-
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
@@ -59,27 +68,36 @@ export default function IssueModal() {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-sm">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900"
                 >
-                  Payment successful
+                  Create new issue
                 </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent you
-                    an email with all of the details of your order.
-                  </p>
+                <div className="mt-5">
+                  <form className="flex flex-col gap-5">
+                    {/* Issue Name */}
+                    <label className="flex gap-3 items-center text-gray-500">
+                      Name:
+                      <input
+                        type="text"
+                        className="bg-slate-200 rounded-sm p-1"
+                        value={issue}
+                        onChange={handleChange}
+                      />
+                    </label>
+                    {/* Issue type */}
+                  </form>
                 </div>
 
-                <div className="mt-4">
+                <div className="mt-6">
                   <button
                     type="button"
                     className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                    onClick={closeModal}
+                    onClick={addIssue}
                   >
-                    Got it, thanks!
+                    Create
                   </button>
                 </div>
               </div>
