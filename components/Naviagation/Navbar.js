8b0bb309fe24e7a6eb959ffd4c8f4f028fab864c
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-28 16:54:56 +0545

    add UI components in navbar

--- components/Naviagation/Navbar.js
+++ components/Naviagation/Navbar.js
@@ -1,13 +1,18 @@
-import * as MdIcons from "react-icons/md";
+import { MdMenu } from "react-icons/md";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 function Navbar({ setSidebarOpen }) {
   return (
-    <div className="flex justify-start items-center my-3 ml-5 lg:hidden">
-      <MdIcons.MdMenu
+    <div className="flex items-center justify-between my-3 mx-5 lg:hidden">
+      <MdMenu
         size={35}
         onClick={() => setSidebarOpen(true)}
         className="hover:cursor-pointer"
       />
+      <div className="flex items-center gap-8">
+        <IoMdNotificationsOutline size={29} />
+        <div className="bg-gray-500 w-8 h-8 rounded-full"></div>
+      </div>
     </div>
   );
 }
