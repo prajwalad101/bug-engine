6a84a47b77ef8f096344a433e69ce93bdd5e7e44
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-01 17:32:34 +0545

    disable refetchOnWindowFocus property on query client

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,13 @@ import Navbar from "../components/Navigation/Navbar";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   // State for opening and closing the sidebar
