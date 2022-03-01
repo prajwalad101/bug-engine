25607b516f35ff71099269aef158f6e964253ae3
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-01 17:36:16 +0545

    load the first project on main index page visit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import { useRouter } from "next/router";
+import IssueModal from "../components/Modal/IssueModal";
+import Heading from "../components/Project/Heading";
+import Issues from "../components/Project/Issues";
+import useProjects from "../hooks/useProjects";
+
 export default function Home() {
-  return <div className=""></div>;
+  const router = useRouter();
+
+  // // Fetch projects from the api
+  const { isLoading, isError, data, error } = useProjects();
+  const projects = data?.data;
+
+  const project = projects && projects[0];
+
+  if (!project) return null;
+
+  router.push(`/project/${project._id}`);
+  return <div></div>;
 }
