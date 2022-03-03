2a8275425271c50f55dcda48de2df86e56532731
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-03 17:45:48 +0545

    reinstall draftjs-wysiwyg and use it to create text editor

--- pages/project/newIssue.js
+++ pages/project/newIssue.js
@@ -1,7 +1,11 @@
 import TextEditor from "../../components/Project/TextEditor";
 
 function NewIssuePage() {
-  return <div className="max-w-[900px] flex justify-center"></div>;
+  return (
+    <div className="max-w-[900px] flex justify-center">
+      <TextEditor />
+    </div>
+  );
 }
 
 export default NewIssuePage;
