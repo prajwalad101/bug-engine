cfdf3b8a8a284a1e49ba71ac2518a6323d4e17f8
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-06 19:54:16 +0545

    create custom hook to fetch a single issue from the server

---
+++ hooks/useIssue.js
@@ -0,0 +1,20 @@
+import { useQuery } from "react-query";
+
+const getIssueById = async (projectId, issueId) => {
+  const res = await fetch(`/api/project/${projectId}/issue/${issueId}`);
+
+  if (!res.ok) {
+    throw Error("Error while fetching issue with that id");
+  }
+  return res.json();
+};
+
+export default function useIssue(projectId, issueId) {
+  return useQuery(
+    ["issue", projectId, issueId],
+    () => getIssueById(projectId, issueId),
+    {
+      enabled: !!projectId && !!issueId,
+    }
+  );
+}
