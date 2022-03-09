4f64bf2972267b62523f4fc652294c14277df941
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-09 18:21:29 +0545

    setup email based authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,27 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../utils/mongoDb";
+
+import EmailProvider from "next-auth/providers/email";
 
 export default NextAuth({
   // Configure authentication providers
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    EmailProvider({
+      server: {
+        port: 465,
+        host: "smtp.gmail.com",
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
-    // setup more providers later
   ],
 });
