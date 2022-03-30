ac88dc96d1c8e98d8e1ba214bd9f4b333bb78ae3
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-30 10:07:33 +0545

    add basic styling and custom button for signin page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,18 +1,26 @@
 import { getProviders, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
+import { AiFillGithub } from "react-icons/ai";
+
 export default function SignIn({ providers }) {
   const router = useRouter();
   return (
     <div>
       {Object.values(providers).map((provider) => {
         return (
-          <div key={provider.name}>
+          <div
+            key={provider.name}
+            className="flex items-center justify-center h-[100vh]"
+          >
             <button
+              type="button"
+              className="flex gap-3 font-raleway  items-center text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2"
               onClick={() =>
                 signIn(provider.id, { callbackUrl: router.query.callbackUrl })
               }
             >
+              <AiFillGithub size={25} />
               Sign in with {provider.name}
             </button>
           </div>
