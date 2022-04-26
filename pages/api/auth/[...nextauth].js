1dfd94170f033e482e07d514088447c65d14aaed
Prajwal Adhikari <prajwalad101@gmail.com>
2022-04-26 22:01:48 +0545

    add a credential provider for custom signing in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,8 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../utils/mongoDb";
 
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import VerifiedUser from "../../../models/VerifiedUser";
 import dbConnect from "../../../utils/dbConnect";
@@ -17,12 +19,26 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Demo user",
+      async authorize(credentials) {
+        console.log("Cred", credentials);
+        return credentials;
+      },
+    }),
   ],
   pages: {
     signIn: "/auth/signin",
   },
   callbacks: {
     async signIn({ user }) {
+      if (user.demo === "true") {
+        return true;
+      }
       // Fetch user from the email
       const userEmail = user?.email;
 
@@ -39,9 +55,16 @@ export default NextAuth({
       if (user && user.role) {
         token.role = user.role;
       }
+      if (user && user.demo) {
+        token.role = "demo";
+      }
       return token;
     },
-    async session({ session, token, user }) {
+    async session({ session, token }) {
+      if (token.role === "demo") {
+        session.user.name = "Demo user";
+        session.user.email = "demouser@gmail.com";
+      }
       session.user.role = token.role;
       session.user.id = token.sub;
       return session;
